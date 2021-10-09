8f725a1227ae1da54116347cd65ae4ad6b209396
rr-10 <remorejithomas@gmail.com>
2021-10-10 00:39:29 +0100

    Initial commit

---
+++ Source/GroupProject/GroupProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GroupProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GroupProject, "GroupProject" );
